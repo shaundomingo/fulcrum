84153d5aab93dd1884ebf964848c548ba22e45db
Shaun Domingo <shaun@ninefold.com>
2013-08-24 16:55:06 +1000

    Commit fulcrum config file

---
+++ config/fulcrum.rb
@@ -0,0 +1,13 @@
+Configuration.for('fulcrum') do
+  # Set this to the domain name of your installation.  Env var APP_HOST
+  #app_host 'example.com'
+
+  # The email address that notification emails will be sent from.  Env var
+  # MAILER_SENDER
+  #mailer_sender 'noreply@example.com'
+
+  # Disable registration pages.  If set to true, users will need to be invited
+  # to a project rather than being able to self sign-up.
+  # Env var DISABLE_REGISTRATION
+  #disable_registration false
+end
